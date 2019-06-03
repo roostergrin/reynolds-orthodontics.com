884d165d391cab77031a49b5c281b65b518145e3
luketevers <luketevers@gmail.com>
2019-06-03 09:44:04 -0700

    creates(style-guide): creates component to demonstrate lazy loading and grid;

--- src/pages/styleguide/styleguide.vue
+++ src/pages/styleguide/styleguide.vue
@@ -1,7 +1,12 @@
 <template lang="pug" src="./styleguide.pug"></template>
 
 <script>
+import StyleGuide from 'components/style-guide/style-guide'
+
 export default {
-  props: ['props']
+  props: ['props'],
+  components: {
+    StyleGuide
+  }
 }
 </script>
