6d3539bb2c57c47a5dca7cc4fbd7e279ca06842a
luketevers <luketevers@gmail.com>
2019-07-01 16:33:52 -0700

    updates(pages): adds components for pages;

--- src/pages/new-patient/new-patient.vue
+++ src/pages/new-patient/new-patient.vue
@@ -1,6 +1,9 @@
 <template lang="pug" src="./new-patient.pug"></template>
 
 <script>
+const BlockTitleText = () => import('components/block/block-title-text/block-title-text')
+const CustomNewForms = () => import('components/custom/custom-new-forms/custom-new-forms')
+const HeroComponent = () => import('components/hero/hero')
 
 export default {
   computed: {
@@ -9,6 +12,9 @@ export default {
     }
   },
   components: {
+    BlockTitleText,
+    CustomNewForms,
+    HeroComponent
   }
 }
 </script>
