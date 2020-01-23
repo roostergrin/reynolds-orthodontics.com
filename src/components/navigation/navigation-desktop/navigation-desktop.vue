efb89a5a1fb8f674f85c6f495ac5c06409a7e7fd
Cooper Timmerman <coopert925@gmail.com>
2020-01-23 10:44:34 -0800

    request(navigation-desktop|app): #29210 -- moved consult button when nav-desktop is fixed

--- src/components/navigation/navigation-desktop/navigation-desktop.vue
+++ src/components/navigation/navigation-desktop/navigation-desktop.vue
@@ -1,6 +1,9 @@
 <template lang="pug" src="./navigation-desktop.pug"></template>
 
 <script>
+
+import BlockExam from 'block/block-exam/block-exam'
+
 export default {
   data () {
     return {
@@ -23,6 +26,9 @@ export default {
       this.$store.dispatch('SET_TAB', i)
       this.dropdownActive = false
     }
+  },
+  components: {
+    BlockExam
   }
 }
 </script>
