5aff4820543bad53982e9e39685347eccdd2b3a1
David Ramirez <davidraviles@gmail.com>
2020-01-27 12:56:13 -0800

    request(index)#29257: modified Google Tag Manager script.

--- dist/index.php
+++ dist/index.php
@@ -6,13 +6,13 @@
   <?php wp_head(); ?>
   <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB105NxpkVIDtjJeUTEt-9Ahtb0Qpb1Djo&amp"></script>
   
-  <!-- Google Tag Manager --> 
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f) (https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f)) ; 
-  })(window,document,'script','dataLayer','GTM-MT5JS5J');</script> 
-  <!-- End Google Tag Manager -->
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MT5JS5J');</script>
+<!-- End Google Tag Manager -->
   
 
 </head>
