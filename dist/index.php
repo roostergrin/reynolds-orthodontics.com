09389d212a383ea6491ce81ddb5fba0256ad60b9
David Ramirez <david.ramirez@roostergrin.com>
2020-03-09 16:05:13 -0700

    request(index)#31351: added script for Swell.

--- dist/index.php
+++ dist/index.php
@@ -13,7 +13,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MT5JS5J');</script>
 <!-- End Google Tag Manager -->
-  
+
+<script>(function(){var w=window;var d=document;var l=function(){var s=d.createElement('script');
+  s.type='text/javascript';s.async=true;
+  s.src="https://platform.swellcx.com/widget/js/chat.js#36838a91-c4a7-4929-b4aa-d016270588b3";
+  s.setAttribute('data-api-token','36838a91-c4a7-4929-b4aa-d016270588b3');
+  var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};
+  if(w.attachEvent){w.attachEvent('onload',l);}
+  else{w.addEventListener('load',l,false);}})();
+</script>  
 
 </head>
 <body>
