e4a26696e01d214adfc83e6ab56e2ac827622cd4
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2023-04-25 09:29:11 -0700

    reverting previous commit

--- dist/functions/email.php
+++ dist/functions/email.php
@@ -15,7 +15,7 @@ function rg_serve_route () {
 
   $data = json_decode(file_get_contents("php://input"), true);
   $from = 'info@wordpress.com';
-  $to = 'reynolds.vm2@myamazingsmile.com';
+  $to = 'info@reynolds-ortho.com';
   $subject = 'API Contact Form';
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $message = '<html><body>';
