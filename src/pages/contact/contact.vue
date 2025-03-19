c5108b02b674d8b38598bc8ba3754b91c33badd6
jason murphy <jmurphy.1196@gmail.com>
2025-03-19 12:13:21 -0700

    requests updates contact forms

--- src/pages/contact/contact.vue
+++ src/pages/contact/contact.vue
@@ -3,6 +3,7 @@
 <script>
 import ContactSection from 'custom/custom-contact-section/custom-contact-section'
 import HeroComponent from 'components/hero/hero'
+import CustomHomeLocation from 'custom/custom-home-location/custom-home-location'
 
 export default {
   computed: {
@@ -12,7 +13,8 @@ export default {
   },
   components: {
     ContactSection,
-    HeroComponent
+    HeroComponent,
+    CustomHomeLocation
   }
 }
 </script>
